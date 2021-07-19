9ea2cba4f889d734052ded6267d6240f00c5935f
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-07-19 17:24:07 +0100

    ft--header component

---
+++ pages/components/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Image from 'next/image';
+import { 
+    BellIcon,
+    HomeIcon,
+    ChatIcon,
+    UserGroupIcon,
+    ViewGridIcon,
+    ChevronDownIcon
+} from '@heroicons/react/solid';
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon
+} from '@heroicons/react/outline';
+
+import HeaderIcon from './HeaderIcon';
+
+function Header() {
+    return (
+        <div className="flex items-center sticky top-0 z-50 bg-white p-2 lg:px-5 shadow-md">
+            {/* Left */}
+            <div>
+                <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" />
+            </div>
+            <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+                <SearchIcon className="h-6 text-gray-600" />
+                <input 
+                    className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+                    type="text"
+                    placeholder="Search Facebook" />
+            </div>
+            {/* Center */}
+            <div className="flex justify-center flex-grow text-black">
+                <div className="flex space-x-6 md:space-x-2">
+                    <HeaderIcon Icon={HomeIcon}/>
+                    <HeaderIcon Icon={FlagIcon}/>
+                    <HeaderIcon Icon={PlayIcon}/>
+                    <HeaderIcon Icon={ShoppingCartIcon}/>
+                    <HeaderIcon Icon={UserGroupIcon}/>
+                </div>
+            </div>
+            {/* Right */}
+        </div>
+    )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from './components/Header'
 
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
+      <Header />
       <main>
         {/* Sidebar */}
         {/* Feed */}
